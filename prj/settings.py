46dbc167dd0089e4ed4e785104682dd794b91e5c
soheej <jangsohee23@naver.com>
2019-04-29 10:39:15 +0900

    Changed ...

--- prj/settings.py
+++ prj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'l$#a4a%)pe=!e=dncp2gax%**wv3!fz&(t*hpvg-me=l^yk4i1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.pythonanywhere.com']
 
 
 # Application definition
